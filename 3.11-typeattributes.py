3955e9bcaaf31c553d91a402ad44c46238b72c9f
AleksanderGm <aleksgrimo@hotmail.com>
2021-12-22 20:00:20 +0100

    type-attributes

---
+++ 3.11-typeattributes.py
@@ -0,0 +1,11 @@
+#for å vite hva du kan gjøre spesifikke typer
+#For eksempel:
+#dir(list), dir(str), dir(float), dir(int)
+
+#For å lære om hva de forskjellige kommandoene gjør for hver type kan du bruke:
+#help(str.upper), help(str.lower) etc.
+
+#Ser du må bruke () for å printe ut .upper
+hello = "ggwp".upper()
+
+print(hello)
---
+++ 3.7-listtypes.py
@@ -0,0 +1,17 @@
+# [] objekter går innen disse i lister
+student_grades = [9.1, 8.8, 7.5]
+
+print(student_grades)
+
+#for å vite hva du kan gjøre spesifikke typer
+#For eksempel:
+#dir(list), dir(str), dir(float), dir(int)
+
+#For å lære om hva de forskjellige kommandoene gjør for hver type kan du bruke:
+#help(str.upper), help(str.lower) etc.
+
+#Ser du må bruke () for å printe ut .upper
+hello = "ggwp".upper()
+
+print(hello)
+
