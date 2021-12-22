8600e019b3d862b9c32dc94e69f6fd80c32209ca
AleksanderGm <aleksgrimo@hotmail.com>
2021-12-22 20:22:40 +0100

    3.18-Dictypes

---
+++ basics/3.18-dictionarytypes.py
@@ -0,0 +1,19 @@
+monday_temperatures = [9.1, 8.8, 7.5]
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+
+#   "Items" i lister er:
+#   "9.1"(øverst)
+#   "Items" i dictionary er:
+#   ""Marry": 9.1"
+
+#   Her vil "9.1" være value
+#   Og ""Marry"" er en key
+
+#mysum = sum(student_grades)
+#length = len(student_grades)
+#mean = mysum / length
+#print(mean)
+
+
+print(student_grades.keys())
+print(student_grades.values())
