b3599d4e63ff10e857f77e52d98caee14cc6684c
AleksanderGm <aleksgrimo@hotmail.com>
2021-12-22 19:22:01 +0100

    variables

---
+++ variables-31.py
@@ -0,0 +1,15 @@
+import datetime
+variable = datetime.datetime.now()
+print(variable)
+
+import datetime
+-variable = datetime.datetime.now()
+print(-variable)
+
+import datetime
+1variable = datetime.datetime.now()
+print(1variable)
+
+mynumber = 10
+mytext = "Hello"
+print(mynumber, mytext)
