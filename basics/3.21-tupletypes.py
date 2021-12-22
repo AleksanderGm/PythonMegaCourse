7ef1a946f8de9c6da044d99fb5df4288785d4d6c
AleksanderGm <aleksgrimo@hotmail.com>
2021-12-22 20:33:38 +0100

    3.21-tuple-types

---
+++ basics/3.21-tupletypes.py
@@ -0,0 +1,18 @@
+#list:
+monday_temperatures = [1, 4, 5]
+print(monday_temperatures)
+
+#dict:
+tuesday_temperatures = {"hei": 8, "hello": 15, "hallo": 20}
+
+#tuple:
+wednesday_temperatures = (1, 4, 5)
+print(wednesday_temperatures)
+
+#forskjellen mellom tuple og list er at i en list kan du legge til ting i listen etter du har lagd
+#i en tuple kan du ikke legge til ting etter du har lagd
+#og det går fra [] til (), list til tuple
+
+monday_temperatures.append(6)
+print(monday_temperatures)
+
