fcd35cd9222effe541672c675524e912187a58ac
AleksanderGm <aleksgrimo@hotmail.com>
2021-12-22 19:47:30 +0100

    Basics-integer-string-float

--- 3.4-basicsISF.py
+++ 3.4-basicsISF.py
@@ -1,7 +1,16 @@
+#integer
 x = 10
+
+#string
 y = "10"
+
+#float
+z = 10.1
 
 sum1 = x + x
 sum2 = y + y
 
 print(sum1, sum2)
+
+#For å finne ut hva slags "class" de over er, for eksempel float/integer/string
+print(type(x), type(y), type(z))
