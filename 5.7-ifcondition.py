42819ebdeadd21944d53b9f746635f7d14b5a636
aleksandergm <aleksandgm@uia.no>
2021-12-25 19:05:11 +0100

    conditions-practice

--- 5.7-ifcondition.py
+++ 5.7-ifcondition.py
@@ -11,3 +11,24 @@ student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
 monday_temperatures = [1.0, 4.0, 5.0]
 print(mean(student_grades))
 print(mean(monday_temperatures))
+
+if 10 < 1:
+    print("Not greater")
+else:
+    print("Greater")
+
+x = 10
+y = 10
+if x < y:
+    print("x is Greater than y")
+elif x == y:
+    print("x is the same as y")
+else:
+    print("x is Smaller than y")
+
+
+message = "Hello there"
+if "Hello" in message:
+    print("Hi")
+else:
+    print("I dont understand")
